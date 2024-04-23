b1b4782f9c1e00436b0545f7118d4d4da5947533
Dmitriy <dimoniche@gmail.com>
2024-04-23 23:43:05 +0300

    Клиентские приложения

--- web/frontend_net_port/src/pages/Main.js
+++ web/frontend_net_port/src/pages/Main.js
@@ -17,6 +17,8 @@ import Grid from '@mui/material/Grid';
 import { StyledTableCell, StyledTableRow } from '../theme/TableTheme';
 
 import net_port from '../files/net_port.service';
+import arm_linux_gnueabihf from '../files/module_net_port_client-0.0.1_arm-linux-gnueabihf';
+import x64_arm_linux from '../files/module_net_port_client-0.0.1_x64-linux';
 
 const Main = () => {
 
@@ -65,7 +67,7 @@ const Main = () => {
                 <TableCell component="th" scope="row">
                     <b>Клиент ARM 32 бит Linux</b>
                 </TableCell>
-                <TableCell align="right">{'нет'}</TableCell>
+                <TableCell align="right">{<a href={arm_linux_gnueabihf} download="module_net_port_client-0.0.1"> module_net_port_client-0.0.1 </a>}</TableCell>
             </StyledTableRow>
             <StyledTableRow
                 sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
@@ -79,9 +81,17 @@ const Main = () => {
                 sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
             >
                 <TableCell component="th" scope="row">
-                    <b>Клиент x64 Linux</b>
+                    <b>Клиент x86 Linux</b>
                 </TableCell>
                 <TableCell align="right">{'нет'}</TableCell>
+            </StyledTableRow>
+            <StyledTableRow
+                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+            >
+                <TableCell component="th" scope="row">
+                    <b>Клиент x64 Linux</b>
+                </TableCell>
+                <TableCell align="right">{<a href={x64_arm_linux} download="module_net_port_client-0.0.1"> module_net_port_client-0.0.1 </a>}</TableCell>
             </StyledTableRow>
             </TableBody>
             </Table>
