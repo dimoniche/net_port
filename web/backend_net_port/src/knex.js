3b89421963248f4511883e04a7e2c2c86834a457
dimoniche <dimoniche@gmail.com>
2026-01-08 14:10:51 +0300

    fix: another psql port

--- web/backend_net_port/src/knex.js
+++ web/backend_net_port/src/knex.js
@@ -12,7 +12,7 @@ module.exports = function (app) {
   const client = 'pg';
   const connection = {
     host: process.env.DB_HOST || 'localhost',
-    port: 5432,
+    port: 15432,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: 'net_port'
